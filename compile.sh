eae84860b2f0097a678b4e46eba72e314b4a1f50
Jim C. Williams <107096799+jcwml@users.noreply.github.com>
2022-10-22 13:20:20 +0100

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,8 +1,8 @@
 clear;clear;
 rm train
 rm aim
-gcc train.c -lm -Ofast -mavx -mfma -o train
+gcc train.c -lm -Ofast -mfma -o train
 ./train
-clang aim.c -Ofast -mavx -mfma -lX11 -lm -o aim
+clang aim.c -Ofast -mfma -lX11 -lm -o aim
 ./aim
 
